cc01233fd5b5bb863398bd566cd5914628a9c206
Danny <dannydo286@gmail.com>
2022-10-23 16:22:27 -0700

    Add initial files for close menu component
    Close menu button functionality will be added in a later update

---
+++ src/components/closeMenuButton.js
@@ -0,0 +1,18 @@
+import { addClass, createElement } from "../helper.js";
+import closeMenuIcon from "../icons/close.svg";
+
+function closeMenuButton() {
+  let closeMenuButton = createElement("button"),
+    icon = createElement("img");
+
+  icon.setAttribute("src", closeMenuIcon);
+  icon.setAttribute("alt", "Close menu icon");
+
+  addClass(closeMenuButton, "close-menu-button");
+
+  closeMenuButton.append(icon);
+
+  return closeMenuButton;
+}
+
+export { closeMenuButton };
