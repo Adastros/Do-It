4484477249c8981021f4f8785c38d4169c37a179
Danny <dannydo286@gmail.com>
2022-12-30 23:24:45 -0800

    Remove theme button and icon images
    Closes #61

--- src/components/appHeader/appHeader.js
+++ src/components/appHeader/appHeader.js
@@ -3,22 +3,19 @@ import { iconButtonMedium } from "../generalButtons/iconButtonMedium.js";
 import { textButton } from "../generalButtons/textButton.js";
 import { addClass, createElement } from "../helper/helper.js";
 import menuIcon from "../../icons/menu.svg";
-import lightIcon from "../../icons/light-theme.svg";
 
 function appHeader() {
   let appHeader = createElement("header"),
     menuButton = iconButtonMedium(menuIcon, "Menu icon"),
-    themeButton = iconButtonMedium(lightIcon, "Light theme icon"),
     addNewTaskButton = textButton("Add task", "add-new-task-button"),
     demoButton = textButton("Demo", "demo-button"),
     topRightButtonContainer = createElement("div");
 
   addClass(menuButton, "menu-button");
-  addClass(themeButton, "theme-button");
   addClass(topRightButtonContainer, "top-right-side-button-container");
   addClass(appHeader, "appHeader");
 
-  topRightButtonContainer.append(addNewTaskButton, demoButton, themeButton);
+  topRightButtonContainer.append(addNewTaskButton, demoButton);
   appHeader.append(menuButton, appLogo(), topRightButtonContainer);
 
   return appHeader;
